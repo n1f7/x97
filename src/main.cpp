f8406d865f927dcc07907ae59790e14cc5c63a7c
Elisey Shemyakin <elisey@shemyakin.me>
2024-03-21 19:47:54 +0300

    Initial cpp implementation

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iterator>
+#include <boost/asio.hpp>
+#include <x97.h>
+#include <cstring>
+#include <vector>
+#include <iomanip>
+
+int main(int argc, char **argv) {
+	std::vector<std::uint16_t> arguments{0x224, 0x96};
+	X97::Packet proto{X97::Packet::Command::GetRegs};
+	proto.appendArguments(std::begin(arguments), std::end(arguments));
+	std::cout << std::setfill('0') << std::hex << std::setw(2);
+	std::copy_n(reinterpret_cast<const char *>(&proto), proto.length(), std::ostream_iterator<unsigned short>(std::cout, " "));
+	std::cout << std::dec << "\nlength:" << proto.length() << '\n';
+}
